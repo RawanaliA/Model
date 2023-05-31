15d158c24c97799ef467c94a894024ac652ce998
RawanaliA <101329672+RawanaliA@users.noreply.github.com>
2023-05-31 11:19:50 +0300

    Add files via upload

---
+++ Model/Resume.java
@@ -0,0 +1,28 @@
+package com.example.jobhub.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class Resume {
+    @Id
+    private Integer resumeID;
+    private String resumName;
+    private String email;
+    private String education;
+    private String experince;
+    private String skill;
+
+    @OneToOne
+    @MapsId
+    @JsonIgnore
+    private Seekers seekers;
+}
